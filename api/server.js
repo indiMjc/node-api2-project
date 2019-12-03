dbcddd234d5d6198060728b5fc15d7d9627f6737
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-03 15:40:45 -0500

    server running with initial endpoint

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+// const postRouter = require("../data/db");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send(`
+    <h2>API up and running<h2>
+    `);
+});
+
+// server.use("/api/posts", postRouter);
+
+module.exports = server;
